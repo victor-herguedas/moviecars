4964420087243c0c4777a8d45565fe62192a964d
Victor Herguedas <victor22junio@gmail.com>
2025-01-21 23:29:53 +0100

    feature: Update IT test to add deadDate

--- src/test/java/com/lauracercas/moviecards/integrationtest/repositories/ActorJPAIT.java
+++ src/test/java/com/lauracercas/moviecards/integrationtest/repositories/ActorJPAIT.java
@@ -12,43 +12,46 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Autor: Laura Cercas Ramos
- * Proyecto: TFM Integración Continua con GitHub Actions
- * Fecha: 04/06/2024
+ * Autor: Laura Cercas Ramos Proyecto: TFM Integración Continua con GitHub
+ * Actions Fecha: 04/06/2024
  */
 @DataJpaTest
 public class ActorJPAIT {
 
-    @Autowired
-    private ActorJPA actorJPA;
+	@Autowired
+	private ActorJPA actorJPA;
 
-    @Test
-    public void testSaveActor() {
-        Actor actor = new Actor();
-        actor.setName("actor");
-        actor.setBirthDate(new Date());
-        actor.setCountry("spain");
+	@Test
+	public void testSaveActor() {
+		final Actor actor = new Actor();
+		actor.setName("actor");
+		actor.setBirthDate(new Date());
+		actor.setDeadDate(new Date(102, 5, 22, 0, 0, 0));
+		actor.setCountry("spain");
 
-        Actor savedActor = actorJPA.save(actor);
+		final Actor savedActor = this.actorJPA.save(actor);
 
-        assertNotNull(savedActor.getId());
+		assertNotNull(savedActor.getId());
 
-        Optional<Actor> foundActor = actorJPA.findById(savedActor.getId());
+		final Optional<Actor> foundActor = this.actorJPA.findById(savedActor.getId());
 
-        assertTrue(foundActor.isPresent());
-        assertEquals(savedActor, foundActor.get());
-    }
+		assertTrue(foundActor.isPresent());
+		assertEquals(savedActor, foundActor.get());
+		assertEquals(new Date(102, 5, 22, 0, 0, 0), foundActor.get().getDeadDate());
+	}
 
-    @Test
-    public void testFindById() {
-        Actor actor = new Actor();
-        actor.setName("actor");
-        actor.setBirthDate(new Date());
-        Actor savedActor = actorJPA.save(actor);
+	@Test
+	public void testFindById() {
+		final Actor actor = new Actor();
+		actor.setName("actor");
+		actor.setBirthDate(new Date());
+		actor.setDeadDate(new Date(102, 5, 22, 0, 0, 0));
+		final Actor savedActor = this.actorJPA.save(actor);
 
-        Optional<Actor> foundActor = actorJPA.findById(savedActor.getId());
+		final Optional<Actor> foundActor = this.actorJPA.findById(savedActor.getId());
 
-        assertTrue(foundActor.isPresent());
-        assertEquals(savedActor, foundActor.get());
-    }
+		assertTrue(foundActor.isPresent());
+		assertEquals(savedActor, foundActor.get());
+		assertEquals(new Date(102, 5, 22, 0, 0, 0), foundActor.get().getDeadDate());
+	}
 }
